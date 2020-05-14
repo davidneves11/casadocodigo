ad0003de53f27cedf3b46f8a0631ea4db97ec662
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-05-14 10:49:51 -0300

    Organização do Projeto
    npm init
    Instalação do express
    Instalação do nodemon
    criação de mais uma rota
    criação das pastas app e config dentro da pasta src

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const http = require('http');
 
 // Criação do servidor Web
 const servidor = http.createServer(function(req, resp) {
-    resp.end(`
+    let html = '';
+    if (req.url == '/') {
+        html = `
         <html>
             <head>
                 <meta charset="utf-8">
@@ -12,6 +14,18 @@ const servidor = http.createServer(function(req, resp) {
                 <h1> Casa do Código </h1>
             </body> 
         </html>
-    `);
+    `;
+    } else if (req.url == '/livros') //outra rota
+        html = `
+    <html>
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1> Listagem de livros </h1>
+        </body> 
+    </html>
+`;
+    resp.end(html);
 });
 servidor.listen(3000);
