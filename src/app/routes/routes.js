4a2c8b73ee4862f00471d9e4a35d32fc157ba0c1
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-05-14 14:15:07 -0300

    Listagem estática dos livros
    Instalação e utilização do Marko para gerar templates HTML

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -16,17 +16,9 @@ module.exports = function(app) {
     });
 
     app.get('/livros', function(req, resp) {
-        resp.send(
-            `
-                <html>
-                    <head>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1> Listagem de livros </h1>
-                    </body> 
-                </html>
-            `
+        resp.marko(
+            require('../views/livros/lista/lista.marko')
+
         );
     });
 }
