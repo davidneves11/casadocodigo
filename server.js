0008c1bc9417f7766d72d26a0c3cbf25806994ce
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-05-14 11:15:33 -0300

    Implementação do Express
    Express será utilizado para a lidar
    
    Configuração do npm start para utilizar o nodemon.

--- server.js
+++ server.js
@@ -1,31 +1,53 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
+app.listen(3000, function() {
+    console.log('Servidor rodando na porta 3000');
 
-// Criação do servidor Web
-const servidor = http.createServer(function(req, resp) {
-    let html = '';
-    if (req.url == '/') {
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-        </html>
-    `;
-    } else if (req.url == '/livros') //outra rota
-        html = `
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Listagem de livros </h1>
-        </body> 
-    </html>
-`;
-    resp.end(html);
+})
+
+app.get('/', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+            </html>
+        `
+    );
 });
-servidor.listen(3000);
+
+
+// const http = require('http');
+// // Criação do servidor Web
+// const servidor = http.createServer(function(req, resp) {
+//     let html = '';
+//     if (req.url == '/') {
+//         html = `
+//         <html>
+//             <head>
+//                 <meta charset="utf-8">
+//             </head>
+//             <body>
+//                 <h1> Casa do Código </h1>
+//             </body> 
+//         </html>
+//     `;
+//     } else if (req.url == '/livros') //outra rota
+//         html = `
+//     <html>
+//         <head>
+//             <meta charset="utf-8">
+//         </head>
+//         <body>
+//             <h1> Listagem de livros </h1>
+//         </body> 
+//     </html>
+// `;
+//     resp.end(html);
+// });
+// servidor.listen(3000);
