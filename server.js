39461b021f3d3a6497b0aaacf059d3f41ff5f0ab
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-05-14 13:44:00 -0300

    Encapsulamento
    Separação das responsabilidades criando um arquivo responsável pela configuração do express e outras dedicado para as rotas.

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
-const express = require('express');
-const app = express();
+const app = require('./src/config/customExpress');
 
 app.listen(3000, function() {
     console.log('Servidor rodando na porta 3000');
 
 })
-
-app.get('/', function(req, resp) {
-    resp.send(
-        `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body> 
-            </html>
-        `
-    );
-});
-
 
 // const http = require('http');
 // // Criação do servidor Web
