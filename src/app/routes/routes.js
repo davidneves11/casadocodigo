aed8ec7556bdf3f8b930866eefa642ee1d2cd93d
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-05-17 15:47:05 -0300

    Método adiciona()
    Inserção do método adiciona no livroDao.js
    Criação do formulário para o cadastro de livros
    Instalação e utilização do body-parser
    Utilização de um middleware para manipular a requisição

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -44,4 +44,16 @@ module.exports = function(app) {
             .catch(erro => console.log(erro));
 
     });
+
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'))
+    });
+
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        livroDao.adiciona(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro));
+    });
 }
